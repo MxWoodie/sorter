9c513dd911693b112138922b521efdb7d0f05ca9
mxwoodie <maxswation@gmail.com>
2018-09-20 15:29:02 +0300

    smartSorterClass

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.myArray = [];
+    this.compareFunction = function(a, b) {
+      return a - b;
+    };
   }
 
   add(element) {
     // your implementation
+    this.myArray.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.myArray[index];
   }
 
   get length() {
     // your implementation
+    return this.myArray.length;
   }
 
   toArray() {
     // your implementation
+    return this.myArray;
   }
 
   sort(indices) {
     // your implementation
+    let subArray = [];
+
+    for(let i = 0; i < indices.length; i++){
+      subArray.push(this.myArray[indices[i]]);
+    }
+    
+    indices.sort(function(a, b) {
+      return a - b;
+    })
+    subArray.sort(this.compareFunction);
+
+    for(let i=0; i < indices.length; i++){
+      this.myArray[indices[i]] = subArray[i];
+    }
+
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
